01701a6f12eeb3ac0a503233bae0efbd0206f28b
Lautaro Cherro <lautaro.cherro@mercadolibre.com>
2023-06-10 11:35:03 -0300

    Now it works!!!!

--- botardo.py
+++ botardo.py
@@ -10,13 +10,45 @@ from time import sleep
 
 
 def main():
-    load_env_variables('.env')
+    load_env_variables()
     consumer_key = getenv("TW_CONSUMER_KEY")
     consumer_secret = getenv("TW_CONSUMER_SECRET")
+    oauth_token = getenv("TW_OAUTH_TOKEN")
+    oauth_token_secret = getenv("TW_OAUTH_TOKEN_SECRET")
 
     while True:
         sleep_until_next_tweet()
-        make_tweet(consumer_key, consumer_secret)
+        make_tweet(consumer_key, consumer_secret, oauth_token, oauth_token_secret)
+
+
+def make_tweet(consumer_key: str, consumer_secret: str, oauth_token: str, oauth_token_secret: str) -> None:
+
+    payload = {"text": get_tweet_content()}
+
+    # Make the request
+    oauth = OAuth1Session(
+        consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=oauth_token,
+        resource_owner_secret=oauth_token_secret,
+    )
+
+    # Making the request
+    response = oauth.post(
+        "https://api.twitter.com/2/tweets",
+        json=payload,
+    )
+
+    if response.status_code != 201:
+        raise Exception(
+            "Request returned an error: {} {}".format(response.status_code, response.text)
+        )
+
+    print("Response code: {}".format(response.status_code))
+
+    # Saving the response as JSON
+    json_response = response.json()
+    print(dumps(json_response, indent=4, sort_keys=True))
 
 
 def sleep_until_next_tweet():
@@ -36,8 +68,8 @@ def sleep_until_next_tweet():
     sleep(sleep_duration)
 
 
-def load_env_variables(file_path):
-    with open(file_path, 'r') as f:
+def load_env_variables():
+    with open(".env", 'r') as f:
         for line in f:
             if line.strip() and not line.startswith('#'):
                 key, value = line.strip().split('=', 1)
@@ -95,75 +127,7 @@ def fancy_days_left(days_left: str) -> str:
     return fancy_days_left
 
 
-def make_tweet(consumer_key: str, consumer_secret: str) -> bool:
-    # Content to tweet
-    payload = {"text": get_tweet()}
-
-    # Get request token
-    request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
-    oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
-    fetch_response = None
-
-    try:
-        fetch_response = oauth.fetch_request_token(request_token_url)
-    except ValueError:
-        print(
-            "There may have been an issue with the consumer_key or consumer_secret you entered."
-        )
-
-    resource_owner_key = fetch_response.get("oauth_token")
-    resource_owner_secret = fetch_response.get("oauth_token_secret")
-    print("Got OAuth token: %s" % resource_owner_key)
-
-    # Get authorization
-    base_authorization_url = "https://api.twitter.com/oauth/authorize"
-    authorization_url = oauth.authorization_url(base_authorization_url)
-    print("Please go here and authorize: %s" % authorization_url)
-    verifier = input("Paste the PIN here: ")
-
-    # Get the access token
-    access_token_url = "https://api.twitter.com/oauth/access_token"
-    oauth = OAuth1Session(
-        consumer_key,
-        client_secret=consumer_secret,
-        resource_owner_key=resource_owner_key,
-        resource_owner_secret=resource_owner_secret,
-        verifier=verifier,
-    )
-    oauth_tokens = oauth.fetch_access_token(access_token_url)
-
-    access_token = oauth_tokens["oauth_token"]
-    access_token_secret = oauth_tokens["oauth_token_secret"]
-
-    # Make the request
-    oauth = OAuth1Session(
-        consumer_key,
-        client_secret=consumer_secret,
-        resource_owner_key=access_token,
-        resource_owner_secret=access_token_secret,
-    )
-
-    # Making the request
-    response = oauth.post(
-        "https://api.twitter.com/2/tweets",
-        json=payload,
-    )
-
-    if response.status_code != 201:
-        raise Exception(
-            "Request returned an error: {} {}".format(response.status_code, response.text)
-        )
-
-    print("Response code: {}".format(response.status_code))
-
-    # Saving the response as JSON
-    json_response = response.json()
-    print(dumps(json_response, indent=4, sort_keys=True))
-
-    return True
-
-
-def get_tweet() -> str:
+def get_tweet_content() -> str:
     days_left, feriado_name, date_str = get_elements(get_html())
     days_left = fancy_days_left(days_left)
     tweet = ""
