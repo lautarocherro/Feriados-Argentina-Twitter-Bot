b9d27323628a39c3c6a3a7cdb0815c5700ecf649
Lautaro Cherro <lautaro.cherro@mercadolibre.com>
2023-06-12 20:35:26 -0300

    Now it's OOP and holiday's are retrieved using an api

---
+++ util.py
@@ -0,0 +1,40 @@
+from random import choice
+
+month_mapping = {
+    1: "Enero",
+    2: "Febrero",
+    3: "Marzo",
+    4: "Abril",
+    5: "Mayo",
+    6: "Junio",
+    7: "Julio",
+    8: "Agosto",
+    9: "Septiembre",
+    10: "Octubre",
+    11: "Noviembre",
+    12: "Diciembre"
+}
+
+fancy_mapping = {
+    "0": "0️⃣",
+    "1": "1️⃣",
+    "2": "2️⃣",
+    "3": "3️⃣",
+    "4": "4️⃣",
+    "5": "5️⃣",
+    "6": "6️⃣",
+    "7": "7️⃣",
+    "8": "8️⃣",
+    "9": "9️⃣"
+}
+
+sad_emoji_list = ("🙄", "😒", "😔", "😢", "😭", "😤", "😐", "🥱", "💀", "⚰️", "🤡")
+weekday_mapping = {
+    "Sunday": f"Domingo",
+    "Monday": "Lunes",
+    "Tuesday": "Martes",
+    "Wednesday": "Miércoles",
+    "Thursday": "Jueves",
+    "Friday": "Viernes",
+    "Saturday": f"Sábado"
+}
